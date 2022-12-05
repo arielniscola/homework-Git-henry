f85985db00b2d8dc9d60c560f414b33e17d5638f
arielniscola <arielniscola@gmail.com>
2022-12-04 22:44:39 -0300

    index.js - funcion

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+    if(numero % 2 === 0){
+        let binario = []
+        while (numero!==0){
+            let residuo = parseInt(numero%2)
+            num = Math.floor(numero/2);
+            binario.push(residuo);
+        }
+        return binario
+    }else{
+        const hexadecimal = numero.toString(16);
+
+        return hexadecimal
     }
+}
